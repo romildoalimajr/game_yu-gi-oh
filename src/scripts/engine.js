85e46d676b7691c496e04172c285411065826474
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-10-30 19:31:06 -0300

    Criando assiniatura das funções principais

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,6 +19,11 @@ const state = {
     },
 };
 
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card",
+}
+
 const pathImages = "../src/assets/icons/";
 
 const cardData = [
@@ -48,8 +53,19 @@ const cardData = [
     },
 ];
 
-function init(){
 
+async function drawCards(cardNumbers, fieldSide){
+    for(let i = 0; i < cardNumbers; i++){
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+function init(){
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
 }
 
 init();
