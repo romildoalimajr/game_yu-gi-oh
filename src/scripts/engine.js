5b3fc58b2c97109e79f69cb8efd179eb8abf721e
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-10-30 19:49:02 -0300

    Implementando a lógica de setar cartas em campo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -53,6 +53,29 @@ const cardData = [
     },
 ];
 
+async function getRandomCardId(){
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide){
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "../src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+    if(fieldSide === playerSides.player1){
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    }
+
+    cardImage.addEventListener("mouseover", () => {
+        drawSelectCard(IdCard);
+    });
+}
 
 async function drawCards(cardNumbers, fieldSide){
     for(let i = 0; i < cardNumbers; i++){
@@ -61,6 +84,7 @@ async function drawCards(cardNumbers, fieldSide){
 
         document.getElementById(fieldSide).appendChild(cardImage);
     }
+    return cardImage;
 }
 
 function init(){
