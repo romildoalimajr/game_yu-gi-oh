582f89740773f28ab7ca397229afa62e015dce82
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-10-30 21:06:47 -0300

    Implementando o mouseover

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,11 +1,11 @@
 
 const state = {
-    score:{
+    score: {
         playerScore: 0,
         computerScore: 0,
         scoreBox: document.getElementById("score_points"),
     },
-    cardSprites:{
+    cardSprites: {
         avatar: document.getElementById("card-image"),
         name: document.getElementById("card-name"),
         type: document.getElementById("card-type"),
@@ -14,17 +14,17 @@ const state = {
         player: document.getElementById("player-field-card"),
         compute: document.getElementById("computer-field-card"),
     },
-    actions:{
+    actions: {
         button: document.getElementById("next-duel"),
     },
 };
 
 const playerSides = {
-    player1: "player-field-card",
-    computer: "computer-field-card",
+    player1: "player-cards",
+    computer: "computer-cards",
 }
 
-const pathImages = "../src/assets/icons/";
+const pathImages = "./src/assets/icons/";
 
 const cardData = [
     {
@@ -53,41 +53,49 @@ const cardData = [
     },
 ];
 
-async function getRandomCardId(){
+async function getRandomCardId() {
     const randomIndex = Math.floor(Math.random() * cardData.length);
 
     return cardData[randomIndex].id;
 }
 
-async function createCardImage(IdCard, fieldSide){
+async function createCardImage(IdCard, fieldSide) {
     const cardImage = document.createElement("img");
     cardImage.setAttribute("height", "100px");
-    cardImage.setAttribute("src", "../src/assets/icons/card-back.png");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
     cardImage.setAttribute("data-id", IdCard);
     cardImage.classList.add("card");
 
-    if(fieldSide === playerSides.player1){
+    if (fieldSide === playerSides.player1) {
+        cardImage.addEventListener("mouseover", () => {
+            drawSelectCard(IdCard);
+        });
+
         cardImage.addEventListener("click", () => {
             setCardsField(cardImage.getAttribute("data-id"));
-        });
+        });        
     }
 
-    cardImage.addEventListener("mouseover", () => {
-        drawSelectCard(IdCard);
-    });
+    return cardImage;
 }
 
-async function drawCards(cardNumbers, fieldSide){
-    for(let i = 0; i < cardNumbers; i++){
+async function drawSelectCard(index) {
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;
+    state.cardSprites.type.innerText = "Attribute : " + cardData[index].type;
+}
+
+async function drawCards(cardNumbers, fieldSide) {
+    for (let i = 0; i < cardNumbers; i++) {
         const randomIdCard = await getRandomCardId();
         const cardImage = await createCardImage(randomIdCard, fieldSide);
 
         document.getElementById(fieldSide).appendChild(cardImage);
     }
-    return cardImage;
+    //return cardImage;
 }
 
-function init(){
+function init() {
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
 }
