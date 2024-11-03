0949b7447f7ccda87d43c28b266d26ea2a1c1222
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-03 13:28:02 -0300

    Colocando uma música de fundo(BGM).

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -212,6 +212,10 @@ function init() {
 
     drawCards(5, state.playerSides.player1);
     drawCards(5, state.playerSides.computer);
+
+    const bgm = document.getElementById("bgm");
+
+    bgm.play();
 }
 
 init();
